4cca15363124e12ae0b1c0ac6415827e3e61c2e5
EricEsquivel <ericdesquivel3@gmail.com>
2024-05-23 12:56:14 -0500

    Final website
    Finished badges,
    fixed SQL create query,
    added back overlay,
    integrated font into our css file instead of a new line on every html file

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ def leaderboard():
 
 @app.route("/create")  # navigating to this web page will insert all these values into your database
 def create():
-    sql = db.session.execute(text('INSERT INTO student_homework(studentname,hwCompletedPercent) VALUES ("Candice", 100%), ("Dontrell", 63%), ("Eren", 28%), ("Faith", 48%), ("Gordon", 68%), ("Harry", 55%), ("Jonathan", 64%)'))
+    sql = db.session.execute(text('INSERT INTO student_homework(studentname,hwCompletedPercent) VALUES ("Candice", 100), ("Dontrell", 63), ("Eren", 28), ("Faith", 48), ("Gordon", 68), ("Harry", 55), ("Jonathan", 64), ("Tim", 50)'))
     db.session.commit()
     return "Entries added."
 
